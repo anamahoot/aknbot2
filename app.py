1771a1efd9d53c26628bc116610d88eb0fc53194
anamahoot <63446230+anamahoot@users.noreply.github.com>
2022-08-07 23:09:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def webhook():
         print("CF>>", symbol,">>",action, ">>Qty=",Qty_buy, " ", COIN,">>USDT=", round(usdt,3))
         Qty_buy = round(Qty_buy,qty_precision)
         print('qty buy : ',Qty_buy)
-        client.futures_change_leverage(symbol=symbol,leverage=lev)
+        client.futures_change_leverage(symbol=symbol,leverage=lev) 
         print('leverage : ',lev)
         order_BUY = client.futures_create_order(symbol=symbol, side='BUY', type='MARKET', quantity=Qty_buy)
         print(symbol," : BUY")
@@ -169,25 +169,27 @@ def webhook():
                 qty_close=round(percent*posiAmt/100,qty_precision)                
                 usdt=round(qty_close*ask,qty_precision)                
                 print("SELL/CloseLong by % amount=", qty_close, " ", COIN, ">> USDT=",round(usdt,3))
+                unRealizedProfit = float(client.futures_position_information(symbol=symbol)[0]['unRealizedProfit'])*percent
             if amount[0]=='$':
                 usdt=float(amount[1:len(amount)])                
                 qty_close = round(usdt/ask,qty_precision)                
                 print("SELL/CloseLong by USDT amount=", usdt, ">> COIN", round(qty_close,3))
+                unRealizedProfit = float(client.futures_position_information(symbol=symbol)[0]['unRealizedProfit'])*usdt/(posiAmt*float(client.futures_position_information(symbol=symbol)[0]['markPrice']))
             print("CF>>", symbol,">>", action, ">> Qty=", qty_close, " ", COIN,">>USDT=", round(usdt,3))                    
             #qty_close = float(client.futures_position_information(symbol=symbol)[0]['positionAmt'])
-            markP =float(client.futures_position_information(symbol=symbol)[0]['markPrice'])*qty_close
+          #  markP =float(client.futures_position_information(symbol=symbol)[0]['markPrice'])*qty_close
             entryP=float(client.futures_position_information(symbol=symbol)[0]['entryPrice'])*qty_close
-            profit=(markP-entryP)
-            print("entry price=",entryP)
-            print("mark price=",markP)
-            print("profit USDT=",profit)
-            unRealizedProfit = float(client.futures_position_information(symbol=symbol)[0]['unRealizedProfit'])  
-            print("unRealizedProfit=",unRealizedProfit)
+         #   profit=(markP-entryP)
+         #   print("entry price=",entryP)
+         #   print("mark price=",markP)
+          #  print("profit USDT=",profit)
+              
+          #  print("unRealizedProfit=",unRealizedProfit)
             #check leverage
             leverage = float(client.futures_position_information(symbol=symbol)[0]['leverage'])  
-            print("leverage=",leverage)
+           # print("leverage=",leverage)
             margin=entryP/leverage
-            print("Enter margin=",margin)
+           # print("Enter margin=",margin)
             ROI=100*unRealizedProfit/margin
             print("ROI%=",ROI)
             
